cdf97b068d8c23bc6c0093459673613af61e0c2f
nolbertog <nolberto.en@gmail.com>
2024-03-25 13:18:19 -0300

    Try-Catch

---
+++ math.js
@@ -0,0 +1,3 @@
+// Generar un número aleatorio entre 1 y 10
+var numeroAleatorio = Math.floor(Math.random() * 10) +1;
+console.log(numeroAleatorio);
---
+++ tryCtach.js
@@ -0,0 +1,12 @@
+
+try{
+    sum =  1 + p
+    console.log(sum)
+} catch (error){
+    
+    const p = 10;
+    sum = 1 + p;
+
+    console.log(sum)
+}
+
